e09a376b194088134d0308583ef4be48d80b5728
ofirt87 <ofirta@checkpoint.com>
2026-08-14 12:47:49 +0300

    Add histogram visualization

--- ofir.py
+++ ofir.py
@@ -1,1 +1,9 @@
-print ("we have one more hour")
+import matplotlib.pyplot as plt
+
+data = [1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 7, 8, 8, 9]
+
+plt.hist(data, bins=5, edgecolor="black")
+plt.title("Histogram")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.show()
